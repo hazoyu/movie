fa4a45d3d13cfae01863dd935e4ffc5f2c66d132
hazoyu <2080686540@qq.com>
2024-09-11 22:49:41 +0800

    底部

---
+++ src/views/Layout/components/Footer.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="footer">
+    <span>商务合作邮箱：luwenbo@qq.com 客服电话：110 违法和不良信息/涉未成年人有害信息举报电话：110</span>
+    <span>用户举报/涉未成年人有害信息举报邮箱：luwenbo@qq.com  舞弊线索举报邮箱：luwenbo@qq.com </span>
+    <span>©2024 文波电影 </span>
+  </div>
+</template>
+
+<style  scoped>
+  .footer{
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100px;
+    background: #262426;
+    text-align: center;
+    color: #ccc;
+    padding: 20px;
+  }
+</style>
